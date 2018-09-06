cb305d4b94b64fb7f3a546a28448825adab96f70
DannyBoyNYC <DannyBoyNYC>
2018-09-06 13:57:11 -0400

    done

---
+++ box-model-inspector/scripts.js
@@ -0,0 +1,9 @@
+var textSpan = document.querySelector('p span');
+var kittenPic = document.querySelector('img');
+var clickedKitten = 0;
+
+kittenPic.addEventListener('click', function(){
+  clickedKitten = clickedKitten + 1
+  console.log(clickedKitten);
+  textSpan.textContent = clickedKitten;
+})
