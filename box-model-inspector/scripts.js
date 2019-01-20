21053ba5c62731b6b237449971118f3bd4f15afa
DannyBoyNYC <daniel.deverell@gmail.com>
2019-01-20 10:34:29 -0500

    done

--- box-model-inspector/scripts.js
+++ box-model-inspector/scripts.js
@@ -1,8 +1,13 @@
+// select portion of the document and store them
 var textSpan = document.querySelector('p span');
 var kittenPic = document.querySelector('img');
+
+// initialize a variable
 var clickedKitten = 0;
 
+// set up the kittenPic to listen for clicks
 kittenPic.addEventListener('click', function () {
+  // and run commands when the kitten is clicked
   clickedKitten = clickedKitten + 1
   console.log(clickedKitten);
   textSpan.textContent = clickedKitten;
